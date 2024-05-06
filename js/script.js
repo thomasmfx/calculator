959e684022e43368059e2f814b7445b08048eb47
Thomas Moisés <thomasmoisesf@gmail.com>
2024-05-06 16:48:16 -0300

    Feat(homepage): add respective functions to most buttons
    Also styled special buttons with another color to better distinction

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-function operate(n1, operand, n2) {
-    let result;
-    switch (operand) {
+const display = document.querySelector("#display-digits");
+const numbers = document.querySelectorAll(".numbers");
+const specialBtns = document.querySelectorAll(".special-btns");
+const times = document.querySelector(".times");
+
+function operate(n1, operation, n2) {
+    let result = 0;
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+
+    switch (operation) {
         case 'plus':
             result = (n1 + n2)
             break;
@@ -10,11 +18,91 @@ function operate(n1, operand, n2) {
         case 'times':
             result = (n1 * n2)
             break;
-        case 'divided':
-            result = (n1 *n2) 
+        case 'division':
+            result = (n1 * n2) 
     }
 
-    return result
+    return isNaN(result) ? n1 : result
+
 }
 
-console.log(operate(20, 'times', 5))
+let n1 = 0;
+let n2 = 0;
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if(display.textContent.length < 10) {
+            display.textContent += parseInt(number.textContent)
+        }
+    })
+});
+
+let lastPressedBtn = '';
+
+specialBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const erase = document.querySelector(".erase");
+        const dot = document.querySelector(".dot");
+        const button = btn.textContent;
+        
+        switch (button) {
+            case ' CE ':
+                display.textContent = null;
+                n1 = 0;
+                n2 = 0;
+                break;
+            case ' C ':
+                display.textContent = null;
+                n2 = 0;
+                break;
+            case ' +/- ':
+                display.textContent = -display.textContent;
+                break;
+            case ' ÷ ':
+                
+                break;
+            case times:
+                n1 = display.textContent;
+                display.textContent = null;
+                lastPressedBtn = times;
+                break;
+            case ' - ':
+                n1 = display.textContent;
+                display.textContent = null;
+                lastPressedBtn = ' - ';
+                break;
+            case ' + ':
+                n1 = display.textContent;
+                display.textContent = null;
+                lastPressedBtn = ' + ';
+                break;
+            case dot:
+                display.textContent += '.';
+                break;
+            case erase:
+                alert(display.textContent)
+                display.textContent = display.textContent.slice(0, -1)
+                break;
+            case ' = ':
+                if (lastPressedBtn == ' + ') {
+                    n2 = display.textContent;
+                    display.textContent = operate(n1, 'plus', n2)
+                } else if(lastPressedBtn == ' - ') {
+                    n2 = display.textContent;
+                    display.textContent = operate(n1, 'minus', n2)
+                } else if (lastPressedBtn == times) {
+                    n2 = display.textContent;
+                    display.textContent = operate(n1, 'multiply', n2)
+                } else if (lastPressedBtn == ' ÷ ') {
+                    n2 = display.textContent;
+                    display.textContent = operate(n1, 'division', n2)
+                }
+        }
+            
+    });
+});
+
+
+
+
+
